4013b69223ee3fbf5006c2dc4b27d4a5f6a48d79
LostFire <hopperlopip@mail.ru>
2024-03-21 01:26:53 +0300

    Добавьте файлы проекта.

---
+++ GameObjectHierarchyTransfer/MainWindow.cs
@@ -0,0 +1,10 @@
+namespace GameObjectHierarchyTransfer
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
